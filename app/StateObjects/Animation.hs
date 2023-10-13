5e269bb322ad248fda9d34e05ad3d9c236fff055
Nemijt03 <Tijmenpvis@gmail.com>
2023-10-13 20:36:28 +0200

    Asteroids only for logic, not data types

--- app/StateObjects/Animation.hs
+++ app/StateObjects/Animation.hs
@@ -1,2 +1,9 @@
 module Animation where
-    
+
+data Animation = DeathAnimation | SpawnAnimation
+                deriving (Show, Eq)
+newtype DeathAnimation = MkDeathAnimation Int
+                deriving (Show, Eq)
+newtype SpawnAnimation = MkSpawnAnimation Int
+                deriving (Show, Eq)
+-- maybe dus functie voor animation :: Int -> Picture
--- app/StateObjects/Enemy.hs
+++ app/StateObjects/Enemy.hs
@@ -1,1 +1,28 @@
 module Enemy where
+
+data Enemy = Asteroid | Saucer 
+                deriving (Show, Eq)
+data Asteroid = MkAsteroid {
+                asteroidPosition :: Point, 
+                asteroidSpeed :: Vector,
+                asteroidSize :: Size,
+                asteroidHealth :: Int
+                }
+                deriving (Show, Eq)
+data Saucer = MkSaucer {
+                saucerPosition :: Point,
+                saucerSpeed :: Vector,
+                saucerAcceleration :: Vector,
+                saucerSize :: Size,
+                saucerHealth :: Int,
+                saucerReloadTime :: Int
+                }
+                deriving (Show, Eq)
+data Size = Small | Medium | Large | ExtraLarge
+                deriving (Show, Eq)
+
+enemiesToPicture :: [Enemy] -> Picture
+enemiesToPicture es = Pictures (enemiesToPictures es)
+
+enemiesToPictures :: [Enemy] -> [Picture] -- These functions can be implemented in the Enemy.hs file
+enemiesToPictures = undefined
