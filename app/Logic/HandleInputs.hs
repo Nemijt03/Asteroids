38a267fdc5e7cea447bc462b46d22acd59034b83
StefanHerald <s.herald@students.uu.nl>
2023-10-18 12:22:58 +0200

    added Assoc

--- app/Logic/HandleInputs.hs
+++ app/Logic/HandleInputs.hs
@@ -1,9 +1,12 @@
 module HandleInputs where
 
+import Assoc
 import Graphics.Gloss.Interface.IO.Game
 
-type Assoc k v  = [(k, v)]
 type Inputs = Assoc Key UserAction
+
+data UserAction = TurnLeft | TurnRight | Forward | Backward | Pause
+            deriving (Eq, Show)
 
 standardInputs :: Inputs
 standardInputs = [(Char 'a'         , TurnLeft  ), 
@@ -12,14 +15,3 @@ standardInputs = [(Char 'a'         , TurnLeft  ),
                 (Char 's'         , Backward ), 
                 (SpecialKey KeyEsc, Pause    )]
 
-search :: Key -> Inputs -> UserAction
-search c list = head [x | (y,x) <- list, y == c]
-
-updateInputs :: Key -> UserAction -> Inputs -> Inputs
-updateInputs c u = foldr f e
-    where
-        f (x, y) xs | y == u    = (c,y) : xs
-                    | otherwise = (x,y) : xs 
-        e = []
-data UserAction = TurnLeft | TurnRight | Forward | Backward | Pause
-            deriving (Eq, Show)
