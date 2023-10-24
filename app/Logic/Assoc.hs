9ea646b92e3058ad6111cbc778ed103d1cbc9b25
StefanHerald <s.herald@students.uu.nl>
2023-10-24 11:50:55 +0200

    changed AND tested Assoc

--- app/Logic/Assoc.hs
+++ app/Logic/Assoc.hs
@@ -3,15 +3,15 @@ module Assoc where
 type Assoc k v  = [(k, v)]
 
 
-search :: k -> Assoc k v -> Maybe v --if the key is not in Inputs, it should return nothing.
-search k list = foldr f Nothing
+search ::Eq k => k -> Assoc k v -> Maybe v --if the key is not in the Assoc, it should return nothing.
+search k list = foldr f Nothing list
     where
         f (k', u) may = case may of
             Nothing -> if k == k' then Just u else Nothing
             _       -> may
 
 
-updateInputs :: k -> v -> Assoc k v -> Assoc k v
+updateInputs :: (Eq k, Eq v) => k -> v -> Assoc k v -> Assoc k v
 updateInputs k v = foldr f []
     where
         f (x, y) xs | y == v    = (k,y) : xs
